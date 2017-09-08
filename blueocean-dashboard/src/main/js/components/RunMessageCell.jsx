73b96b68a5d18cd0eaa4dca670509d6838db3f71
Nicolae Pascu <contact@nicupascu.ro>
2017-09-08 18:27:46 +1000

    add link to Jira ticket in RunMessageCell

--- blueocean-dashboard/src/main/js/components/RunMessageCell.jsx
+++ blueocean-dashboard/src/main/js/components/RunMessageCell.jsx
@@ -28,18 +28,38 @@ export default class RunMessageCell extends Component {
             message = (<span className="RunMessageCell" title={run.description}><span className="RunMessageCellInner">{run.description}</span></span>);
         } else if (showCommitMessage) {
             const commitMsg = run.changeSet[run.changeSet.length - 1].msg;
+            let commitMsgWithIssues = [];
             if (run.changeSet[run.changeSet.length - 1].issues) {
                 console.log(run.changeSet[run.changeSet.length - 1].issues);
+                let issuesIdString = '';
+                let issuesObj = {};
+
+                for (const issue of run.changeSet[run.changeSet.length - 1].issues) {
+                    issuesIdString += `${issue.id}|`;
+                    issuesObj[issue.id] = issue.url;
+                }
+
+                let issuesRegExpString = new RegExp(`(${issuesIdString.slice(0, -1)})`, 'gi');
+
+                for (let commitMsgPart of commitMsg.split(issuesRegExpString)) {
+                    if (issuesObj[commitMsgPart]) {
+                        commitMsgWithIssues.push(<a href={issuesObj[commitMsgPart]} target="_blank">{commitMsgPart}</a>);
+                    } else {
+                        if (commitMsgPart) {
+                            commitMsgWithIssues.push(commitMsgPart);
+                        }
+                    }
+                }
             }
             if (run.changeSet.length > 1) {
                 return (
                     <span className="RunMessageCell" title={commitMsg}>
-                        <span className="RunMessageCellInner">{commitMsg}</span>
+                        <span className="RunMessageCellInner">{commitMsgWithIssues ? commitMsgWithIssues : commitMsg}</span>
                         <Lozenge title={t('lozenge.commit', { 0: run.changeSet.length })} />
                     </span>
                 );
             }
-            return (<span className="RunMessageCell" title={commitMsg}><span className="RunMessageCellInner">{commitMsg}</span></span>);
+            return (<span className="RunMessageCell" title={commitMsg}><span className="RunMessageCellInner">{commitMsgWithIssues ? commitMsgWithIssues : commitMsg}</span></span>);
         } else if (showCauses) {
             // Last cause is always more significant than the first
             const cause = run.causes[run.causes.length - 1].shortDescription;
